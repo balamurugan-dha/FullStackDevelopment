25aed8b66d718dcf9af461856eb7f52ae95f932e
Balamurugan <balamurugan.dha@gmail.com>
2021-06-05 13:02:54 +0530

    Created a simple project

---
+++ SimpleProject/src/FirstProgram.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Dbalamurugan
+ *
+ */
+public class FirstProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is a test run...");
+
+	}
+
+}
